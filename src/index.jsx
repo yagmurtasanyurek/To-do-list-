063754af7c2aa31de4fabeb97669847337efdc91
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-04-25 18:43:39 +0300

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+const el = document.getElementById("root");
+const root = createRoot(el);
+
+root.render(<App />);
