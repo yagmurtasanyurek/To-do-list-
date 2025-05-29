d7371599847d584daf1bf5bda721f721032d5158
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-05-30 00:07:50 +0300

    delete: absolute positioning

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,15 @@ function App() {
 
   return (
     <div className=" min-h-screen bg-neutral-200">
-      <div className=" relative h-[40vh] w-full z-0 bg-[url(./image/mountain2.jpg)] bg-cover bg-center bg-no-repeat">
-        -
-      </div>
-      {/*   <div className="flex justify-center"> */}
-      <div
-        className="absolute  left-1/2 top-1/2  translate-x-[-50%] translate-y-[-90%] z-10 
-           w-2/5 "
-      >
-        <Header />
-        <TaskInput />
-        <TaskList />
+      <div className=" h-[40vh] w-full z-0 bg-[url(./image/mountain2.jpg)] bg-cover bg-center bg-no-repeat"></div>
+      <div className="flex justify-center">
+        <div className=" z-10 -mt-56 w-2/5 ">
+          <Header />
+          <TaskInput />
+          <TaskList />
+        </div>
       </div>
     </div>
-    /*    </div> */
   );
 }
 
